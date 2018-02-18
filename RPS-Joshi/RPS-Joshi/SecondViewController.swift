94e23bd71179498cc87d58345b44e10c235c2887
Joshi,Aawaj R <s521315@ol10014m.nwmissouri.edu>
2018-02-18 16:50:36 -0600

    Made the basic UI.

---
+++ RPS-Joshi/RPS-Joshi/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RPS-Joshi
+//
+//  Created by Joshi,Aawaj R on 2/18/18.
+//  Copyright © 2018 Joshi,Aawaj R. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
